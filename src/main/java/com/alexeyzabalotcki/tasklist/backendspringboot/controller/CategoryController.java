f4dacd00f2c77482fbddbe112e183ad1a21c721a
AlexeyZabalotcki <AlexeyZabalotckiBusiness@gmail.com>
2022-09-20 14:28:46 +0300

    Added update and add methods

--- src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/controller/CategoryController.java
+++ src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/controller/CategoryController.java
@@ -2,9 +2,9 @@ package com.alexeyzabalotcki.tasklist.backendspringboot.controller;
 
 import com.alexeyzabalotcki.tasklist.backendspringboot.entity.Category;
 import com.alexeyzabalotcki.tasklist.backendspringboot.repository.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +19,37 @@ public class CategoryController {
     }
 
     @GetMapping("/test")
-    public List<Category> test(){
+    public List<Category> test() {
 
         List<Category> list = categoryRepository.findAll();
 
         return list;
     }
+
+    @PostMapping("/add")
+    public ResponseEntity<Category> add(@RequestBody Category category) {
+
+        if (category.getId() != null && category.getId() != 0) {
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (category.getTitle() != null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Category> update(@RequestBody Category category) {
+
+        if (category.getId() == null || category.getId() == 0) {
+            return new ResponseEntity("Error: id MUST be fill", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: you MUST fill title form", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
 }
--- src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/controller/PriorityController.java
@@ -2,9 +2,9 @@ package com.alexeyzabalotcki.tasklist.backendspringboot.controller;
 
 import com.alexeyzabalotcki.tasklist.backendspringboot.entity.Priority;
 import com.alexeyzabalotcki.tasklist.backendspringboot.repository.PriorityRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,16 +14,44 @@ public class PriorityController {
 
     private PriorityRepository priorityRepository;
 
-
     public PriorityController(PriorityRepository priorityRepository) {
         this.priorityRepository = priorityRepository;
     }
 
     @GetMapping("/test")
-    public List<Priority> test(){
+    public List<Priority> test() {
 
         List<Priority> list = priorityRepository.findAll();
 
         return list;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Priority> add(@RequestBody Priority priority) {
+
+        if (priority.getId() != null && priority.getId() != 0) {
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getTitle() != null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Priority> update(@RequestBody Priority priority) {
+
+        if (priority.getId() == null || priority.getId() == 0) {
+            return new ResponseEntity("Error: id MUST be fill", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: you MUST fill title form", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("missed param: you MUST fill color form", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 }
