1ab42c5ddf86b878a64db9c12db2343b6535f25d
AlexeyZabalotcki <AlexeyZabalotckiBusiness@gmail.com>
2022-09-21 13:53:19 +0300

    Created search queries, changed controllers and added classes containers for easier search

--- src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/repository/PriorityRepository.java
+++ src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/repository/PriorityRepository.java
@@ -2,12 +2,19 @@ package com.alexeyzabalotcki.tasklist.backendspringboot.repository;
 
 import com.alexeyzabalotcki.tasklist.backendspringboot.entity.Priority;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PriorityRepository extends JpaRepository<Priority, Long> {
+
+    @Query("SELECT p from Priority p where " +
+            "(:title is null or :title='' or lower(p.title) like lower(concat('%',:title, '%'))) " +
+            "order by p.title asc")
+    List<Priority> findByTitle(@Param("title") String title);
 
     List<Priority> findAllByOrderByIdAsc();
 
