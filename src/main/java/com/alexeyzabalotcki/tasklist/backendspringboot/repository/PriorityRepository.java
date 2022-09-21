7d1e3b38696f3f1149072e75ebbbce38d2997ac4
AlexeyZabalotcki <AlexeyZabalotckiBusiness@gmail.com>
2022-09-21 11:51:59 +0300

    Created methods findAll in Category an Priority Controllers, also created methods findAllByOrderByIdAsc in PriorityRepository and findAllByOrderByTitleAsc in CategoryRepository

--- src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/repository/PriorityRepository.java
+++ src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/repository/PriorityRepository.java
@@ -4,6 +4,11 @@ import com.alexeyzabalotcki.tasklist.backendspringboot.entity.Priority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PriorityRepository extends JpaRepository<Priority, Long> {
+
+    List<Priority> findAllByOrderByIdAsc();
+
 }
