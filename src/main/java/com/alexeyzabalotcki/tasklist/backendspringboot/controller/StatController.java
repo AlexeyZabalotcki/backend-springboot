a758b8107851405813cd40caf66ec72beaabcb17
AlexeyZabalotcki <AlexeyZabalotckiBusiness@gmail.com>
2022-10-17 19:15:28 +0300

    Added annotation allowing connect to frontend application

--- src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/controller/StatController.java
+++ src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/controller/StatController.java
@@ -1,14 +1,15 @@
 package com.alexeyzabalotcki.tasklist.backendspringboot.controller;
 
 import com.alexeyzabalotcki.tasklist.backendspringboot.entity.Stat;
-import com.alexeyzabalotcki.tasklist.backendspringboot.repository.StatRepository;
 import com.alexeyzabalotcki.tasklist.backendspringboot.service.StatService;
 import com.alexeyzabalotcki.tasklist.backendspringboot.util.MyLogger;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class StatController {
     private final StatService statService;
 
--- src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/controller/TaskController.java
+++ src/main/java/com/alexeyzabalotcki/tasklist/backendspringboot/controller/TaskController.java
@@ -17,6 +17,7 @@ import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/task")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TaskController {
 
     private final TaskService taskService;
